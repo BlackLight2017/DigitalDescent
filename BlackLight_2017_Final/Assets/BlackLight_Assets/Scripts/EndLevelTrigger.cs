38462e59c55b4968dd0f2ae817eb75c79feedeb9
JezusGaming <jezz331@gmail.com>
2017-10-09 17:26:02 +1100

    Added end level trigger

---
+++ BlackLight_2017_Final/Assets/BlackLight_Assets/Scripts/EndLevelTrigger.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndLevelTrigger : MonoBehaviour {
+
+    public Canvas End;
+    private bool m_fEnd = false;
+    private SelectOnInput Select;
+
+    // Use this for initialization
+    void Start () {
+        End.enabled = false;
+        Select = End.GetComponent<SelectOnInput>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(m_fEnd)
+        {
+            Select.enabled = true;
+        }
+        else
+        {
+            Select.enabled = false;
+        }
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == ("Player"))
+        {
+            End.enabled = true;
+            m_fEnd = true;
+        }
+        else
+        {
+            End.enabled = false;
+            m_fEnd = false;
+        }
+    }
+}
