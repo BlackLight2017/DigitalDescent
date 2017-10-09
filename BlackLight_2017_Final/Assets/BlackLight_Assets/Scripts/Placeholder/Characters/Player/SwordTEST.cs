5974c905cd02ae9f4753a52b6be6630f9b10a46b
JezusGaming <jezz331@gmail.com>
2017-10-09 14:34:15 +1100

    Gave Player sword a attack delay

--- BlackLight_2017_Final/Assets/BlackLight_Assets/Scripts/Placeholder/Characters/Player/SwordTEST.cs
+++ BlackLight_2017_Final/Assets/BlackLight_Assets/Scripts/Placeholder/Characters/Player/SwordTEST.cs
@@ -6,8 +6,13 @@ public class SwordTEST : MonoBehaviour
 {
     //// Damage of the attack a
     public float fDamage = 15.0f;
-
+    public float m_fAttackTime = 1.0f;
     public AudioSource swingsound;
+
+    private bool Attacking = false;
+    private bool RangedAttacking = false;
+    Enemy EnemyScript;
+    RangedEnemy RangedEnemyScript;
 
     //// The speed of the animation (cant be used yet)
     // public float fSpeed = 2.0f;
@@ -16,39 +21,59 @@ public class SwordTEST : MonoBehaviour
     // public float force = 5;
     private void FixedUpdate()
     {
-        
-      
+        m_fAttackTime -= Time.deltaTime;
+        if (XCI.GetButton(XboxButton.X))
+        {
+            if (Attacking && m_fAttackTime <= 0)
+            {
+                EnemyScript.TakeDamage(fDamage);
+                m_fAttackTime = 1;
+            }
+            if (RangedAttacking && m_fAttackTime <= 0)
+            {
+                RangedEnemyScript.TakeDamage(fDamage);
+                m_fAttackTime = 1;
+            }
+            
+        }
     }
 
-    
+
     private void OnTriggerEnter(Collider other)
     {
         //   if (other.gameObject.tag == "Enemy" )//&& (Input.GetKey(KeyCode.F)))
         //   {
         //  if (Input.GetKey(KeyCode.F) )
         //    if (Input.GetAxis("Attack") > 0)
-        if (XCI.GetButton(XboxButton.X))
+        
+
+        EnemyScript = other.GetComponent<Enemy>();
+        if (EnemyScript != null)
         {
-
-            Enemy EnemyScript = other.GetComponent<Enemy>();
-            if (EnemyScript != null)
+            Debug.Log("HitEnemy");
+            if (EnemyScript.m_fHealth > 0)
             {
-                Debug.Log("HitEnemy");
-                if (EnemyScript.m_fHealth > 0)
-                {
-                    EnemyScript.TakeDamage(fDamage);
-                }
+                Attacking = true;
             }
+            else
+                Attacking = false;
+        }
+        else
+            Attacking = false;
 
-            RangedEnemy RangedEnemyScript = other.GetComponent<RangedEnemy>();
-            if (EnemyScript != null)
+        RangedEnemyScript = other.GetComponent<RangedEnemy>();
+        if (EnemyScript != null)
+        {
+            Debug.Log("HitEnemy");
+            if (RangedEnemyScript.m_fHealth > 0)
             {
-                Debug.Log("HitEnemy");
-                if (RangedEnemyScript.m_fHealth > 0)
-                {
-                    RangedEnemyScript.TakeDamage(fDamage);
-                }
+                RangedAttacking = true;
             }
+            else
+                RangedAttacking = false;
+        }
+        else
+            RangedAttacking = false;
 
             //// Enemy equals Gameobjects with the tag "Enemy" 
             //GameObject Enemy12 = GameObject.FindGameObjectWithTag("Enemy");
@@ -56,7 +81,7 @@ public class SwordTEST : MonoBehaviour
             //Enemy enemytest = Enemy12.GetComponent<Enemy>();
 
             //enemytest.TakeDamage(fDamage);
-        }
+        
     
     ///   // Enemy equals Gameobjects with the tag "Enemy" 
     ///   GameObject Enemy12 = GameObject.FindGameObjectWithTag("Enemy");
