917cee17c0842795d28e09f39086a40fc0e56f85
JezusGaming <jezz331@gmail.com>
2017-10-11 11:52:38 +1100

    Fixed fast camera turn speed

--- BlackLight_2017_Final/Assets/BlackLight_Assets/Scripts/Placeholder/Characters/Player/CameraFollow.cs
+++ BlackLight_2017_Final/Assets/BlackLight_Assets/Scripts/Placeholder/Characters/Player/CameraFollow.cs
@@ -3,8 +3,10 @@ using XboxCtrlrInput;
 public class CameraFollow : MonoBehaviour {
     public Transform target;
 
-    public float smoothSpeed = 0.125f;
-    public Vector3 offset; 
+    public float smoothSpeed;
+    public Vector3 offset;
+    public float SpeedChangeTime;
+    private float m_fTmier = 0; 
 
     void FixedUpdate()
     {
@@ -16,11 +18,26 @@ public class CameraFollow : MonoBehaviour {
 
         if (XCI.GetAxis(XboxAxis.LeftStickX) < 0 )
         {
+            m_fTmier += Time.deltaTime;
+            if(m_fTmier >= SpeedChangeTime)
+            {
+                smoothSpeed = 0.06f;
+            }
             offset.x = -5;
         }
         if (XCI.GetAxis(XboxAxis.LeftStickX) > 0)
         {
+            m_fTmier += Time.deltaTime;
+            if (m_fTmier >= SpeedChangeTime)
+            {
+                smoothSpeed = 0.06f;
+            }
             offset.x = 5;
+        }
+        if(XCI.GetAxis(XboxAxis.LeftStickX) == 0)
+        {
+            m_fTmier = 0;
+            smoothSpeed = 0.02f;
         }
     }
 
