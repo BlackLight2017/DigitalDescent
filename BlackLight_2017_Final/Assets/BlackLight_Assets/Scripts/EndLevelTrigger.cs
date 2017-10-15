f31d859f699bad7b1cf421e8d31256cae8ce2e7b
JezusGaming <jezz331@gmail.com>
2017-10-16 09:54:01 +1100

    More comments

--- BlackLight_2017_Final/Assets/BlackLight_Assets/Scripts/EndLevelTrigger.cs
+++ BlackLight_2017_Final/Assets/BlackLight_Assets/Scripts/EndLevelTrigger.cs
@@ -4,22 +4,39 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class EndLevelTrigger : MonoBehaviour {
-
+    //----------------------------------------------------------------------------------------------------
+    // Sets up references to other objects and creates variables
+    //----------------------------------------------------------------------------------------------------
     public Canvas End;
     public bool m_bEnd = false;
 
+    //----------------------------------------------------------------------------------------------------
     // Use this for initialization
-    void Start () {
+    //----------------------------------------------------------------------------------------------------
+    void Start ()
+    {
+        // Sets the canvas to not appear at the start
         End.enabled = false;
 	}
-	
-	// Update is called once per frame
-	void Update ()
+
+    //----------------------------------------------------------------------------------------------------
+    // Update is called once per frame,
+    //----------------------------------------------------------------------------------------------------
+    void Update ()
     {
 	}
 
+    //----------------------------------------------------------------------------------------------------
+    // OnTriggerEnter is called every time it is colliding with another object, It brings up the end
+    // canvas and sets end to true if the player has reached it. Else they are both set to false
+    //
+    // Param: 
+    //      other: Is the object that is being collided with.
+    //----------------------------------------------------------------------------------------------------
     private void OnTriggerEnter(Collider other)
     {
+        // If the player triggers this then the end will happen.
+        // if not nothing happens and after the player has triggered to it resets.
         if(other.gameObject.tag == ("Player"))
         {
             End.enabled = true;
