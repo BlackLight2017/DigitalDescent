6c2562724e417b0296607209298094eaf4f78a18
jakemakesgames <jakebrown.designer@gmail.com>
2017-11-28 11:10:19 +1100

    added the new menu scene

---
+++ BlackLight_2017_Final/Assets/BlackLight_Assets/Scripts/menu/Reset.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class Reset : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        Time.timeScale = 1;
+        Debug.Log(Time.timeScale);
+
+    }
+}
