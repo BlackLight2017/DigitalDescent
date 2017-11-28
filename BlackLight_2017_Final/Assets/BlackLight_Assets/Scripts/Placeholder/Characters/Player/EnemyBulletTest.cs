d4b3cc722ddff37792b15f1fea045e1cb1d8ec3a
Gabriel4224 <31612343+Gabriel4224@users.noreply.github.com>
2017-11-29 09:49:20 +1100

    Commented EnemybulletScirpt

--- BlackLight_2017_Final/Assets/BlackLight_Assets/Scripts/Placeholder/Characters/Player/EnemyBulletTest.cs
+++ BlackLight_2017_Final/Assets/BlackLight_Assets/Scripts/Placeholder/Characters/Player/EnemyBulletTest.cs
@@ -8,6 +8,9 @@ using UnityEngine;
 
 public class EnemyBulletTest : MonoBehaviour
 {
+    //----------------------------------------------------------------------------------------------------
+    // Sets up references to other objects and creates variables
+    //----------------------------------------------------------------------------------------------------
     private float m_fspawn_time = 5;
     private float m_fspawn_timer = 0;
     public float m_fSpeed = 1.0f;
@@ -16,17 +19,19 @@ public class EnemyBulletTest : MonoBehaviour
     PlayerHealth PlayerScripts;
     Vector3 MoveDirection;
 
-
+    //----------------------------------------------------------------------------------------------------
     // Use this for initialization
+    //----------------------------------------------------------------------------------------------------
     void Start()
     {
-        //m_fspawn_timer = spawn_time;
+        // Gets player healthScript
         Player = GameObject.FindGameObjectWithTag("Player");
         if(Player)
             PlayerScripts = Player.GetComponent<PlayerHealth>();
     }
-
-    // Update is called once per frame
+    //----------------------------------------------------------------------------------------------------
+    // Update is called once per frame, in this script the bullet is firing in the direction of the player
+    //----------------------------------------------------------------------------------------------------
     void Update()
     {
         //m_fspawn_timer = spawn_time;
@@ -45,17 +50,23 @@ public class EnemyBulletTest : MonoBehaviour
             transform.position += MoveDirection * m_fSpeed * Time.deltaTime;
         }
     }
-
+    //----------------------------------------------------------------------------------------------------
+    // On collidionEnter is called once theyre is a collision, When the bullet collides 
+    // anything the bullet gets destroyed
+    //----------------------------------------------------------------------------------------------------
     private void OnCollisionEnter(Collision other)
     {
+        // if it collides with the player
 		if (other.gameObject.tag == "Player")
 		{
-		//	//Destroy(gameObject);
-		}
+
+        }
+        // if the bullet collides with enemies or the players sword the bullet doesent get destroyed 
 		if(other.gameObject.tag == "RangedEnemy" || other.gameObject.tag == "Enemy" || other.gameObject.tag == "Gun" || other.gameObject.tag == "EnemyStunGun" || other.gameObject.tag == "Sword")
 		{
 
 		}
+        // if the bullet hits niether of the tags destroy bullet
 		else
 		{
 			Destroy(gameObject);
